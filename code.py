238370b6b1b816e6a06b382a8fffd7cb4c82d966
Rafay Khan <76584894+rafayk330@users.noreply.github.com>
2021-04-15 08:33:26 +0500

    Final update code.py

--- code.py
+++ code.py
@@ -329,27 +329,133 @@ def PollyPlot(xtrain, xtest, y_train, y_test, lr,poly_transform):
     plt.ylim([-10000, 60000])
     plt.ylabel('Price')
     plt.legend()
+y_data = df["price"]
+x_data = df.drop('price',axis=1)
+from sklearn.model_selection import train_test_split
+x_train , x_test , y_train , y_test = train_test_split (x_data , y_data , test_size = 0.10 , random_state = 0)
+from sklearn.model_selection import train_test_split
+x_train1 , x_test1 , y_train1 , y_test1 = train_test_split (x_data , y_data , test_size = 0.40 , random_state = 0)
+from sklearn.linear_model import LinearRegression
+lre = LinearRegression()
+lre.fit(x_train[["horsepower"]],y_train)
+from sklearn.linear_model import LinearRegression
+lr2 = LinearRegression()
+lr2.fit(x_train1[["horsepower"]],y_train1)
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score (lre , x_data[["horsepower"]] , y_data , cv = 4)
+scores
+np.mean(scores)
+scores.std()
+from sklearn.model_selection import cross_val_score
+scores1 = cross_val_score (lr2 , x_data[["horsepower"]] , y_data , cv = 2)
+np.mean (scores)
+from sklearn.model_selection import cross_val_predict
+yhat = cross_val_predict (lre , x_data[["horsepower"]] , y_data , cv = 4)
+yhat[0:5]
+lr = LinearRegression()
+lr.fit(x_train[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']], y_train)
+yhat_train = lr.predict(x_train[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']])
+yhat_train[0:5]
+yhat_test = lr.predict(x_test[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']])
+yhat_train[0:5]
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+Title = 'Distribution  Plot of  Predicted Value Using Training Data vs Training Data Distribution'
+DistributionPlot(y_train, yhat_train, "Actual Values (Train)", "Predicted Values (Train)", Title)
+Title='Distribution  Plot of  Predicted Value Using Test Data vs Data Distribution of Test Data'
+DistributionPlot(y_test,yhat_test,"Actual Values (Test)","Predicted Values (Test)",Title)
+from sklearn.preprocessing import PolynomialFeatures
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.45, random_state=0)
+pr = PolynomialFeatures(degree=5)
+x_train_pr = pr.fit_transform(x_train[['horsepower']])
+x_test_pr = pr.fit_transform(x_test[['horsepower']])
+poly = LinearRegression()   # Create a Model "poly" and train it.
+poly.fit(x_train_pr, y_train)
+yhat = poly.predict(x_test_pr)
+yhat[0:5]
+PollyPlot(x_train[['horsepower']], x_test[['horsepower']], y_train, y_test, poly,pr)
+poly.score(x_test_pr, y_test)
+Rsqu_test = []
 
+order = [1, 2, 3, 4]
+for n in order:
+    pr = PolynomialFeatures(degree=n)
+    
+    x_train_pr = pr.fit_transform(x_train[['horsepower']])
+    
+    x_test_pr = pr.fit_transform(x_test[['horsepower']])    
+    
+    lr.fit(x_train_pr, y_train)
+    
+    Rsqu_test.append(lr.score(x_test_pr, y_test))
 
+plt.plot(order, Rsqu_test)
+plt.xlabel('order')
+plt.ylabel('R^2')
+plt.title('R^2 Using Test Data')
+plt.text(3, 0.75, 'Maximum R^2 ')
+def f(order, test_data):
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=test_data, random_state=0)
+    pr = PolynomialFeatures(degree=order)
+    x_train_pr = pr.fit_transform(x_train[['horsepower']])
+    x_test_pr = pr.fit_transform(x_test[['horsepower']])
+    poly = LinearRegression()
+    poly.fit(x_train_pr,y_train)
+    PollyPlot(x_train[['horsepower']], x_test[['horsepower']], y_train,y_test, poly, pr)
+interact(f, order=(0, 6, 1), test_data=(0.05, 0.95, 0.05))
+pr1 = PolynomialFeatures(degree=2)
+x_train_pr1 = pr.fit_transform(x_train[['horsepower','curb-weight','engine-size','highway-mpg']])  
+x_test_pr1 = pr.fit_transform(x_test[['horsepower','curb-weight','engine-size','highway-mpg']])   
+x_train_pr1.shape
+x_test_pr1.shape
+poly1 = LinearRegression()
+poly1.fit(x_train_pr1,y_train)
+yhat_test1 = poly1.predict(x_test_pr1)
+Title='Distribution  Plot of  Predicted Value Using Test Data vs Data Distribution of Test Data'
+DistributionPlot(y_test, yhat_test1, "Actual Values (Test)", "Predicted Values (Test)", Title)
+pr=PolynomialFeatures(degree=2)
+x_train_pr=pr.fit_transform(x_train[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg','normalized-losses','symboling']])
+x_test_pr=pr.fit_transform(x_test[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg','normalized-losses','symboling']])
+from sklearn.linear_model import Ridge
+RidgeModel = Ridge (alpha = 0.1)
+RidgeModel.fit (x_train_pr , y_train)
+Yhat = RidgeModel.predict (x_train_pr)
+yhat = RidgeModel.predict(x_test_pr)
+Rsqu_test = []
+Rsqu_train = []
+dummy1 = []
+Alpha = 10 * np.array(range(0,1000))
+for alpha in Alpha:
+    RigeModel = Ridge(alpha=alpha) 
+    RigeModel.fit(x_train_pr, y_train)
+    Rsqu_test.append(RigeModel.score(x_test_pr, y_test))
+    Rsqu_train.append(RigeModel.score(x_train_pr, y_train))
+width = 12
+height = 10
+plt.figure(figsize=(width, height))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.plot(Alpha,Rsqu_test, label='validation data  ')
+plt.plot(Alpha,Rsqu_train, 'r', label='training Data ')
+plt.xlabel('alpha')
+plt.ylabel('R^2')
+plt.legend()
+from sklearn.linear_model import Ridge
+RidgeModel = Ridge (alpha = 10)
+RidgeModel.fit (x_train_pr , y_train)
+RidgeModel.score(x_test_pr , y_test)
+from sklearn.model_selection import GridSearchCV
+parameters2= [{'alpha': [0.001,0.1,1, 10, 100, 1000,10000,100000,100000],'normalize':[True,False]} ]
+Grid2 = GridSearchCV(Ridge(), parameters2,cv=4)
+Grid2.fit(x_data[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']],y_data)
+Grid2.best_estimator_
+score = Grid2.cv_results_
+score["mean_test_score"]
+from sklearn.model_selection import GridSearchCV
+parameters1= [{'alpha': [0.001,0.1,1, 10, 100, 1000, 10000, 100000, 100000]}]
+RR=Ridge()
+Grid1 = GridSearchCV(RR, parameters1,cv=4)
+Grid1.fit(x_data[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']], y_data)
+BestRR=Grid1.best_estimator_
+BestRR
+BestRR.score(x_test[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']], y_test)
